df5c54ad7e798f64d3cb9fc3ca1c14670d57564d
pooza <tkoishi@b-shock.co.jp>
2023-05-06 10:52:43 +0900

    #1

--- app/task/bundle.rb
+++ app/task/bundle.rb
@@ -2,12 +2,6 @@ module Paprika
   extend Rake::DSL
 
   namespace :bundle do
-    desc 'install gems'
-    task install: ['paprika:api:bundler', 'paprika:feed:bundler'] do
-      sh 'gem install bundler -v "~>2.0"'
-      sh 'bundle install --jobs 4 --retry 3'
-    end
-
     desc 'update gems'
     task :update do
       sh 'bundle update --jobs 4 --retry 3'
