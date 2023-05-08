53dab6daa28dd1aadd8b5c558a41abf22ca8f150
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2023-05-09 01:26:08 +0900

    #1

--- app/lib/paprika/server.rb
+++ app/lib/paprika/server.rb
@@ -3,9 +3,8 @@ module Paprika
     include Package
     set :root, Environment.dir
 
-    get '/health' do
-      @renderer.message = Environment.health
-      @renderer.status = @renderer.message[:status] || 200
+    get '/check/:name' do
+      @renderer.message = params[:name]
       return @renderer.to_s
     end
   end
